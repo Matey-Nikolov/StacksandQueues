b2d3ca43a72159f9cdc9319351e207afeb3223db
Matey-Nikolov <matey.nikolov@mail.bg>
2021-05-26 16:49:40 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stacks_and_Queues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+
+            int popedElement = stack.Pop(); // Remove and return last element.
+            int peekElement = stack.Peek(); // Return last element without removing
+
+            Console.WriteLine(popedElement);
+            Console.WriteLine(peekElement);
+            Console.WriteLine("*************");
+            Console.WriteLine(string.Join(", ", stack));
+        }
+    }
+}
